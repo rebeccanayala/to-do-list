007c06044e004c338f5075a1a1b26100849ba869
Rebecca Nayala <140104442+rebeccanayala@users.noreply.github.com>
2023-11-17 23:35:31 -0300

    Adding js

---
+++ main.js
@@ -0,0 +1,38 @@
+let tasks = [];
+
+function addTask() {
+    const taskInput = document.getElementById("taskInput");
+    const taskList = document.getElementById("taskList");
+
+    if (taskInput.value.trim() !== ""){
+        tasks.push(taskInput.value);
+        updateTaskList(taskList);
+        taskInput.value = "";
+    }
+}
+
+function removeTask(index) {
+    tasks.splice(index, 1);
+    const taskList = document.getElementById("taskList");
+    updateTaskList(taskList);
+}
+
+function toggleList() {
+    tasks[index] = tasks[index].starsWith("[Concluída] ", "") ?
+        tasks[index].replace("Concluída ", "") :
+        "[Concluída] " + tasks[index];
+    const taskList = document.getElementById("taskList");
+    updateTaskList(taskList);    
+}
+
+function updateTaskList(taskList) {
+    taskList.innerHTML = "";
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+            <span class="${task.startsWith("[Concluída] ") ? "completed" : ""}">${task}</span>
+            <button onclick="toggleTask(${index})">Concluir</button>
+            <button onclick="removeTask(${index})">Remover</button>`;
+        taskList.appendChild(li);
+    });
+}
